0aeb3fba798ea2d27be5a6e6b2049c4a8bea144e
Eduardo Irías <eduardo22i@me.com>
2013-05-06 14:11:57 -0600

    - The 4 calculations are shown.

--- Simulador Cache Ram/SCRAppDelegate.h
+++ Simulador Cache Ram/SCRAppDelegate.h
@@ -7,9 +7,27 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import "SCRCalculator.h"
+
 
 @interface SCRAppDelegate : NSObject <NSApplicationDelegate>
 
 @property (assign) IBOutlet NSWindow *window;
+
+@property (weak) IBOutlet NSScrollView *table;
+@property (weak) IBOutlet NSButton *start_btn;
+
+
+@property (nonatomic) SCRCalculator *sincache;
+@property (nonatomic) SCRCalculator *directo;
+@property (nonatomic) SCRCalculator *asociativo;
+@property (nonatomic) SCRCalculator *asociativo_conj;
+
+@property (weak) IBOutlet NSTextField *lbl_datos_1;
+@property (weak) IBOutlet NSTextField *lbl_datos_2;
+@property (weak) IBOutlet NSTextField *lbl_datos_3;
+@property (weak) IBOutlet NSTextField *lbl_datos_4;
+
+- (IBAction)start:(id)sender;
 
 @end
--- Simulador Cache Ram/SCRAppDelegate.m
+++ Simulador Cache Ram/SCRAppDelegate.m
@@ -8,11 +8,54 @@
 
 #import "SCRAppDelegate.h"
 
+
 @implementation SCRAppDelegate
+
+@synthesize table;
+@synthesize sincache, directo, asociativo, asociativo_conj;
+@synthesize lbl_datos_1, lbl_datos_2, lbl_datos_3, lbl_datos_4;
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     // Insert code here to initialize your application
+      
+    
+
+    
+    
+}
+
+
+- (IBAction)start:(id)sender {
+     
+    sincache = [[SCRCalculator alloc] init];
+    [sincache startwithtype:0];
+    
+    lbl_datos_1.stringValue = [NSString stringWithFormat:@"%f µs",sincache.mips ];
+    
+    directo = [[SCRCalculator alloc] init];
+    [directo startwithtype:1];
+    
+    
+    lbl_datos_2.stringValue = [NSString stringWithFormat:@"%f µs",directo.mips ];
+    
+    asociativo  = [[SCRCalculator alloc] init];
+    [asociativo startwithtype:2];
+    
+    
+    lbl_datos_3.stringValue = [NSString stringWithFormat:@"%f µs",asociativo.mips ];
+    
+    asociativo_conj = [[SCRCalculator alloc] init];
+    [asociativo_conj startwithtype:3];    
+    
+    lbl_datos_4.stringValue = [NSString stringWithFormat:@"%f µs",asociativo_conj.mips ];
+    
+    NSUserNotification *notification = [[NSUserNotification alloc] init];
+    notification.title = @"Calculations Done!";
+    notification.informativeText = @"We have finish the simulations.";
+    notification.soundName = NSUserNotificationDefaultSoundName;
+    
+    [[NSUserNotificationCenter defaultUserNotificationCenter] deliverNotification:notification];
 }
 
 @end
